9070f63f395e65c5636684e0d2e78e699ba2ab3a
Xlroo <80978847+Xlroo@users.noreply.github.com>
2025-03-15 13:09:28 +0300

    Create a Navbar component

---
+++ onlyAgents/src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+import '../App.css'
+
+function Navbar() {
+  
+  return (
+    <>
+       <div className='container'> 
+        <div><h1>Logo</h1></div>
+        <div>React</div>
+        <div>
+          <nav >
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contacts </li>
+              <li>Projects</li>
+            </ul>
+          </nav>
+        </div>
+        </div>  
+
+    </>
+  )
+}
+
+export default Navbar
